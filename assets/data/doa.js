144069839a9880e016cfe5c6198c32d3124b4117
badruti94 <badruakfm@gmail.com>
2020-04-29 19:03:19 +0700

    1

---
+++ assets/data/doa.js
@@ -0,0 +1,45 @@
+const doa = [
+	{
+		nama : "Buka Puasa",
+		arab : "اَللّهُمَّ لَكَ صُمْتُ وَبِكَ آمَنْتُ وَعَلَى رِزْقِكَ أَفْطَرْتُ بِرَحْمَتِكَ يَا اَرْحَمَ الرَّحِمِيْنَ",
+		latin : "Allahumma laka shumtu wa bika amantu wa'ala rizqika afthartu. Birrahmatika yaa arhamar roohimin.",
+		indo : "Ya Allah, untuk Mu aku berpuasa, dan kepada Mu aku beriman, dan dengan rezeki Mu aku berbuka. Dengan rahmat Mu wahai yang Maha Pengasih dan Penyayang.",
+		warna : 1
+	},
+	{
+		nama : "Sebelum Makan",
+		arab : "اَللّٰهُمَّ بَارِكْ لَنَا فِيْمَا رَزَقْتَنَا وَقِنَا عَذَابَ النَّارِ",
+		latin : "Alloohumma barik lanaa fiimaa razatanaa waqinaa 'adzaa bannar",
+		indo : "Ya Allah, berkahilah kami dalam rezeki yang telah Engkau berikan kepada kami dan peliharalah kami dari siksa api neraka",
+		warna : 4
+	},
+	{
+		nama : "Sesudah Makan",
+		arab : "اَلْحَمْدُ ِللهِ الَّذِىْ اَطْعَمَنَا وَسَقَانَا وَجَعَلَنَا مُسْلِمِيْنَ",
+		latin : "Alhamdu lillaahil ladzii ath'amanaa wa saqoonaa wa ja'alnaa muslimiin",
+		indo : "Segala puji bagi Allah yang telah memberi makan kami dan minuman kami, serta menjadikan kami sebagai orang-orang islam",
+		warna : 3
+	},
+	{
+		nama : "Ketika Makan Lupa Membaca Doa",
+		arab : "بِسْمِ اللهِ فِىِ أَوَّلِهِ وَآخِرِهِ",
+		latin : "Bismillaahi fii awwalihi wa aakhirihi",
+		indo : "Dengan menyebut nama Allah pada awal dan akhirnya",
+		warna: 4
+	},
+	{
+		nama : "Sebelum Tidur",
+		arab : "بِسْمِكَ اللّٰهُمَّ اَحْيَا وَاَمُوْتُ",
+		latin : "Bismikallohumma ahya wa amuutu",
+		indo : "Dengan menyebut nama-Mu ya Allah, aku hidup dan aku mati",
+		warna : 5
+	},
+	{
+		nama : "Masuk Kamar Mandi Atau Toilet",
+		arab : "اَللّٰهُمَّ اِنّىْ اَعُوْذُبِكَ مِنَ الْخُبُثِ وَالْخَبَآئِثِ",
+		latin : "Alloohumma Innii a'uudzubika minal khubutsi wal khoaaitsi",
+		indo : "Ya Allah, aku berlindung pada-Mu dari godaan syetan  laki-laki dan setan perempuan",
+		warna : 2
+	},
+
+];
---
+++ assets/tes.js
@@ -0,0 +1,6 @@
+const a = 'a';
+
+const arr = [];
+
+
+const teks = "http://127.0.0.1:5500/surah.html?1#3";
